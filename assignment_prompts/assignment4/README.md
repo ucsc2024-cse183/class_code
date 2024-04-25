5d6fc1beda8cd2ac7e9491a734772b3e3126ae61
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-04-24 18:39:10 -0700

    assignment4

---
+++ assignment_prompts/assignment4/README.md
@@ -0,0 +1,20 @@
+You must complete the provided bird_spotter app. It is a single page app.
+
+The main page is in apps/bird_spotter/static/index.html and in apps/bird_spotter/static/js/index.js
+- `/bird_spotter/` should redirect to `/bird_spotter/static/index.html` or turn it into a template (1 point)
+- in models.py define a database table to store birds, a bird should have an "id", a "name", an "habitat" (string), a "weight" (positive number), and a number of "sightings" (positive number) (1 point)
+- create a POST endpoint `api/birds` to register a new bird as required by index.js (1 point)
+- create a GET endpoint `api/birds` to get birds as required by index.js (1 point)
+- create a POST endpoint `api/birds/{id}/increase_sightings` to increase the number of sightings by 1 (no body in POST) as required by index.js (1 point)
+- create a PUT endpoint `api/birds/{id}` to update bird info as required by index.js (1 point)
+- create a DELETE endpoint `api/birds/{id}` to delete a bird (1 point)
+- wire the API to get birds in the JS (0.5 point)
+- wire the API to register a bird in the JS (use validate_and_insert) (0.5 point)
+- wire the API to increase sightings in the JS (0.5 point)
+- wire the API to update the bird info in the JS (use validate_and_update) (0.5 point)
+- add validators to the table to prevent invalid inputs and duplicated birds (1 point)
+- modify the app to display the errors returned by the JS (for example negative weight) (2 point)
+
+IMPORTANT:
+Do not use authentication (no auth object) else the grader will not work.
+You should use db and session fixtures.
---
+++ assignment_prompts/assignment4/apps/bird_spotter/static/js/index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+function clone(obj) { return JSON.parse(JSON.stringify(obj)); }
+
+let app = {}
+app.empty_new_bird = {"id": 0, "name": "", "habitat": "", "weight": 0, "sightings": 0},
+app.data = {    
+    data: function() {
+        return {
+            new_bird: clone(app.empty_new_bird),            
+            birds: [],
+            selected_birds: [],
+            editing: {current: null}
+        };
+    },
+    methods: {       
+        search: function() {
+            this.cancel();
+            this.selected_birds = this.birds.filter((bird)=>{return bird.name.toLowerCase().includes(this.new_bird.name.toLowerCase()); });
+        },
+        add_bird: function() {
+            this.cancel();
+            if (this.selected_birds.length==0 && this.new_bird.name.length>1) {
+                let bird = clone(this.new_bird);
+                this.birds.push(bird);
+                this.new_bird = app.empty_new_bird;
+                this.selected_birds = [bird];
+                // POST bird to /bird_spotter/birds which returns {id: #} and store it in bird.id = #;
+                // remove the following
+                bird.id = (new Date()).getTime();
+            }            
+        },
+        edit: function(bird) {
+            this.cancel();
+            this.editing = {current: bird, old: clone(bird)};
+        },
+        save: function(bird) {
+            bird = clone(bird);            
+            delete bird.id;
+            delete bird.name;
+            // PUT bird to /bird_spotter/birds/{bird.id}            
+            this.editing = {current: null};
+        },
+        cancel: function() {
+            if (this.editing.current)
+                for(var key in this.editing.current)
+                    this.editing.current[key] = this.editing.old[key];
+            this.editing = {current: null};
+        },
+        add_sighting: function(bird) {
+            bird.sightings += 1;
+            // POST {} to /bird_spotter/birds/{bird.id}/increase_sightings
+        },
+        color: function(name) {            
+            let hash = 0;
+            for (let i = 0; i < name.length; i++) hash = name.charCodeAt(i) + ((hash << 5) - hash);            
+            let ret = `hsl(${(hash % 360)}, 100%, 75%)`;                         
+            console.log(ret);
+            return ret;
+        },
+    }
+};
+app.load_data = function() {
+    // GET from /bird_spotter/birds {birds: [...]} and store it into app.vue.birds = [...]
+}
+
+app.vue = Vue.createApp(app.data).mount("#app");
+app.load_data();
