f9c1cb8d0860dd74257fac78446719fa423d1566
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-05-04 23:44:33 -0700

    print commits

--- grader/task.sh
+++ grader/task.sh
@@ -1,8 +1,13 @@
 set -e
+echo "cleaning up memory..."
 docker stop $(docker ps -a  | grep selenium | awk '{print $1}')	|| true
 docker container prune --force || true
 echo '{"status": "failure"}' > task.output.json
+echo "cloning student repo..."
 git clone https://mdipierro:$(cat ~/token)@github.com/ucsc2024-cse183/$1-code.git > /dev/null
 path=`pwd`/$1-code/$2
+echo "checkouts info..."
+find ./ -name ".git" -type d | sed "s,.git$,," | xargs -i sh -c "cd {}; echo -n {}; git rev-parse HEAD"
+echo "grading..."
 cd class_code/grader
 ./grade.sh $path && echo '{"status": "success"}' > ../../task.output.json
