63b7a6fd9d31608de577fee89b7f3c642c9d167d
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-05-05 16:43:55 -0700

    better repo links

--- grader/task.sh
+++ grader/task.sh
@@ -6,8 +6,8 @@ echo '{"status": "failure"}' > task.output.json
 echo "cloning student repo..."
 git clone https://mdipierro:$(cat ~/token)@github.com/ucsc2024-cse183/$1-code.git > /dev/null
 path=`pwd`/$1-code/$2
-echo "checkouts info..."
-find ./ -name ".git" -type d | sed "s,.git$,," | xargs -i sh -c "cd {}; echo -n {}; git rev-parse HEAD"
+echo "print repos info..."
+find ./ -name ".git" -type d | sed "s,.git$,," | xargs -i sh -c 'cd {}; echo "{}:$(git config --get remote.origin.url)/commit/$(git rev-parse HEAD)"' | sed 's/\/\/.*@/\/\//' | sed 's/\.git//'
 echo "grading..."
 cd class_code/grader
 ./grade.sh $path && echo '{"status": "success"}' > ../../task.output.json
