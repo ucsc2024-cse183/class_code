00d7a8edd630f10f46fab4b0f4782c9e17fd552e
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-05-07 23:38:33 -0700

    change in gredentials logic

--- grader/task.sh
+++ grader/task.sh
@@ -4,7 +4,7 @@ docker stop $(docker ps -a  | grep selenium | awk '{print $1}')	|| true
 docker container prune --force || true
 echo '{"status": "failure"}' > task.output.json
 echo "cloning student repo..."
-git clone https://mdipierro:$(cat ~/token)@github.com/ucsc2024-cse183/$1-code.git > /dev/null
+git clone https://mdipierro:${github_credentials}@github.com/ucsc2024-cse183/$1-code.git > /dev/null
 path=`pwd`/$1-code/$2
 echo "print repos info..."
 find ./ -name ".git" -type d | sed "s,.git$,," | xargs -i sh -c 'cd {}; echo "{}:$(git config --get remote.origin.url)/commit/$(git rev-parse HEAD)"' | sed 's/\/\/.*@/\/\//' | sed 's/\.git//'
