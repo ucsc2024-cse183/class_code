7ee9dff938aa33f32c2b6446745a680b56270976
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-05-20 22:38:54 -0700

    todo with vue component

--- lectures/README.md
+++ lectures/README.md
@@ -1,1 +1,48 @@
-# nothing to see here yet
+# Security
+
+## Access Control
+
+```
+db.define_table("thing", Field("name"), auth.signature)
+
+@action("edit/<thing_id:int>")
+@action.uses(auth.user) # only a logged in use can access
+def edit(thing_id):
+
+    # only selected user can access this
+    selected_users = ["user1", "user2"]
+    user = auth.get_user()
+    if user.username not in selected_users:
+        raise HTTP(401)
+
+    # you want to allow this only if the current user has created 2
+    the_thing = db.thing(thing_id, created_by=auth.user_id)
+    if not the_thing:
+        raise HTTP(401)
+
+    form = Form(db.thing, the_thing)
+    return locals()
+```
+
+
+## SQL Injections
+
+```
+>>> name = "Roland Harper"
+>>> db.executesql("SELECT * FROM person WHERE name='%s';" % name)
+[(1, 'Roland Harper', None, None)]
+>>> name = "' OR ''='"
+>>> db.executesql("SELECT * FROM person WHERE name='%s';" % name)
+... get everyting ...
+```
+
+Always use an API that prevents SQL injections
+```
+>>> name = "' OR ''='"
+>>> print(db(db.person.name == name).select())
+person.id,person.name,person.age,person.color
+
+>>> db._lastsql
+('SELECT "person"."id", "person"."name", "person"."age", "person"."color" FROM "person" WHERE ("person"."name" = \'\'\' OR \'\'\'\'=\'\'\');', 0.00017118453979492188)
+```
+
---
+++ lectures/week8/todo.js
@@ -0,0 +1,89 @@
+"use strict";
+
+// utility function to clone an object
+function clone(obj) { return JSON.parse(JSON.stringify(obj)); }
+
+// container for everything
+var app = {};
+// convenience function to make an item
+app.make_item = function() {
+    return {
+        title: "",
+        description: "",
+        state: 0,        
+    };
+};
+// The vue input config object    
+app.config = {};
+// The vue input setup() function returns the data to be exposed
+app.config.setup = function() {
+    return {
+        new_item: Vue.ref(app.make_item()),
+        items: Vue.ref([]),
+        message: Vue.ref("")
+    };
+};
+// the vue methods to be exposed
+app.config.methods = {};
+// method to mv a new item from the form into the list of items
+app.config.methods.add_new_item = function() {
+    console.log(this);
+    console.log(app.vue);
+    if (app.vue.new_item.title.trim() !== "") {
+        app.vue.items.push(clone(this.new_item));
+        app.vue.new_item = app.make_item();
+    }
+    console.log(JSON.stringify(this.items));
+    console.log(app.vue.items);
+    // app.save();
+};
+app.config.methods.receiveEmit = function(data) {
+    app.vue.items = app.vue.items.filter(function(item)
+    {
+        return item.title != data.title;
+    });
+};
+
+// save and load methods (the save in the browser memory)
+app.save = function() {
+    window.localStorage.setItem("todo_items", JSON.stringify(app.vue.items));
+}
+app.load = function() {
+    try {
+        let items = JSON.parse(window.localStorage.getItem("todo_items"));
+        if (items) app.vue.items = items;
+    } catch(e) {};
+}
+
+// make the vue app
+app.tmp = Vue.createApp(app.config);
+
+// article component
+app.tmp.component("my-article", {
+    props: ["item"],
+    setup: function(vars) {return {}; },
+    emits: ['message'],
+    methods: {
+        // method to rotate (0->1->2->0) the state of an item (to change color)
+        rotate: function(item) {
+            item.state = (item.state + 1) % 3;
+            app.save();
+        },
+        // method to remove the specified item
+        remove: function(item_to_rm) {
+            this.$emit('message', {title: item_to_rm.title});
+            // app.save();
+        }
+    },
+    template: `<article class="message"    
+    v-bind:class="{'is-success':item.state==0, 'is-warning':item.state==1, 'is-danger':item.state==2}">
+    <div class="message-header" v-on:click="rotate(item)">
+    <p v-text="item.title"></p>
+    <button class="delete" aria-label="delete" v-on:click="remove(item)"></button>
+    </div>
+    <div class="message-body" v-text="item.description"></div>
+    </article>`
+});
+app.vue = app.tmp.mount("#myapp");
+// then reload any saved data
+// app.load();
