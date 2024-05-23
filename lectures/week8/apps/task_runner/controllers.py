81fed80bdb922edabfe1f1460d712fe9a53c398c
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-05-22 23:07:24 -0700

    better task runner

--- lectures/week8/apps/task_runner/controllers.py
+++ lectures/week8/apps/task_runner/controllers.py
@@ -1,23 +1,31 @@
 
 from py4web import action, request, abort, redirect, URL, Field
 from yatl.helpers import A
+from pydal.validators import *
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from py4web.utils.form import Form
+from py4web.utils.grid import Grid
 
 @action("index", method=["GET","POST"])
 @action.uses("generic.html", auth.user)
 def index():
-    form = Form([Field("task_name"), Field("sleep_time", "integer")])
+    form = Form([
+        Field("name", rquired=IS_NOT_EMPTY()), 
+        Field("x", "float", requires=IS_FLOAT_IN_RANGE(0,10))])
     if form.accepted:
-        db.mytask.insert(name=form.vars["task_name"],
-                         task_info={"function": "task1", "inputs": {"x": form.vars["sleep_time"]}})
+        db.mytask.insert(name=form.vars["name"],
+                         task="task1",
+                         input={"x": form.vars["x"]},
+                         created_by=auth.user_id)
         # start or record the task
         redirect(URL("task_recorded"))
     return locals()
 
 
 @action("task_recorded")
+@action("task_recorded/<path:path>")
 @action.uses("generic.html", auth.user)
-def task_recorded():
+def task_recorded(path=None):
     message = "you task was recorded"
-    return locals()    
+    grid = Grid(path, db.mytask.created_by==auth.user_id)
+    return locals()
--- lectures/week8/apps/task_runner/models.py
+++ lectures/week8/apps/task_runner/models.py
@@ -4,44 +4,58 @@ This file defines the database models
 
 from .common import db, Field, auth
 from pydal.validators import *
-import threading
+import multiprocessing
 import time
 import sys
 
 
+# example of a task
+# some task that is time consuming here simply add 1 to x
 def task1(x):
     time.sleep(int(x))
     print("done!")
+    return x+1
    
+# register tasks (only one for now)
 TASKS = {"task1": task1}   
 
+# table where to store task queue
 db.define_table(
     "mytask",
-    Field("name"),
-    Field("task_info", "json"),  # <- {"function": "task1", "inputs": {"x": 1}}
-#    auth.signature
+    Field("name"),  # name given by user
+    Field("task"),  # from TASKS
+    Field("input", "json"),  # the input, for example {"x": 1}
+    Field("output", "json"), # the output to be stored after execution
+    Field("status", default="queued"), # change to success or failed
+    Field("created_by", "reference auth_user"),
 )
 
+# function that will process tasks in background
 def process_tasks():
     with open("task_runner.log", "w") as stream:
+        # redirect output to file
+        sys.stdout = stream
+        sys.stderr = stream
         while True:
-            rows = db(db.mytask).select()
-            stream.write(f"found {len(rows)} tasks\n")
+            rows = db(db.mytask.status=="queued").select()
             for row in rows:
-                stream.write(row.name+"\n")
-                task_info = row.task_info
-                func_name = task_info["function"]
-                inputs = task_info["inputs"]
-                TASKS[func_name](**inputs)
-                row.delete_record()
+                print("processing", row.name)                
+                try:
+                    output = {"output": TASKS[row.task](**row.input)}
+                    row.update_record(output=output, status="success")
+                    print("task completed!")
+                except Exception:
+                    print(traceback.format_exc())
+                    output = {"error": traceback.format_exc()}
+                    row.update_record(output=output, status="failed")
+                    print("task failed!")
                 db.commit()
-                sys.stdout.flush()
-            sys.stdout.flush()
-            stream.flush()
+                stream.flush()
             time.sleep(1)
 
-thread = threading.Thread(target=process_tasks)
-thread.start()
+# run tasks in background process or thread
+process = multiprocessing.Process(target=process_tasks)
+process.start()
 
         
 
